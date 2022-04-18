c31ce36ac85aa3068ea7db9c47f01167e0b11b43
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-18 15:28:56 -0400

    Create a reducer to handle pending, fulfilled and rejected stated for fetctWeatherAction call

--- weather_book/src/redux/slices/weatherSlices.js
+++ weather_book/src/redux/slices/weatherSlices.js
@@ -8,11 +8,36 @@ export const fetchWeatherAction = createAsyncThunk(
   async (payload, {rejectWithValue, getState, dispatch}) => {
     try {
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`);
-      return response;
+      return response.data;
     } catch(error) {
-
+      if(!error?.response) {
+        throw error
+      }
+      return rejectWithValue(error?.response?.data);
     }
   }
 );
 
 //createSlice creates action creators and reducers.
+//There are 3 states to be considered for each action type - pending, fulfilled, rejected
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchWeatherAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchWeatherAction.fulfilled, (state, action) => {
+      state.weather = action?.payload; //action && action.payload
+      state.loading = false;
+      state.error = undefined;
+    });
+    builder.addCase(fetchWeatherAction.rejected, (state, action) => {
+      state.loading = false;
+      state.weather = undefined;
+      state.error = action?.payload;
+    })
+  }
+});
+
+export default weatherSlice.reducer;
